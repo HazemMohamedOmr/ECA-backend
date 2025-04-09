33b04f08567d93848fe76527f26a04b4338f14d3
Hazem Mohamed <Hazem.ma.omar@gmail.com>
2025-04-09 10:36:37 +0200

    all

--- ECA-backend/Program.cs
+++ ECA-backend/Program.cs
@@ -9,8 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllWithCredentials", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    });
+});
 
 builder.Services.AddScoped<IPDFService, PDFService>();
 
